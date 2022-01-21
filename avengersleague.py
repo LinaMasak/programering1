d4c398db081ef83e241253618305e80c1c4bd0d7
LinneaBergvall <linnea.adesso@gmail.com>
2022-01-21 11:02:37 +0100

    fixat
    såklart jag vet, här är svaret:)

--- avengersleague.py
+++ avengersleague.py
@@ -8,3 +8,9 @@ hjältar = avengers + dc
 def select_random_Ns(hjältar, n)
     random.shuffle(hjältar)
     result = []
+    for i in range(0, len(hjältar), n):
+    result.append(hjältar[i:i + n])
+    return result
+
+
+print(select_random_Ns(hjältar, 2))
