3df5c3d97524ad040a7f202941b84a99d4740648
LinaMasak <lina.masak@gmail.com>
2022-01-21 11:01:12 +0100

    Uppdatering
    Har fastnat här vet du hur man gör sen

--- avengersleague.py
+++ avengersleague.py
@@ -1,3 +1,10 @@
+import random
+
 avengers = ["Iron man", "Thor", "Spindelmannen", "Hulken", "Captian America" ]
 dc = ["Batman", "Superman", "Flash", "Aquaman", "Superwoman"]
 
+hjältar = avengers + dc
+
+def select_random_Ns(hjältar, n)
+    random.shuffle(hjältar)
+    result = []
